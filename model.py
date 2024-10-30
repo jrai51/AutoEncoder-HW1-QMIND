519e347984518b7696b8546923ac93ef8075b7f5
jrai51 <87856634+jrai51@users.noreply.github.com>
2024-10-30 19:41:07 -0400

    Created skeleton code

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+class autoencoderMLP4Layer(nn.Module):
+
+	def __init__(self, N_input=784, N_bottleneck=8, N_output=784):
+		#
+		#	Your code here ...
+		#
+		pass
+
+	def forward(self, X):
+		#
+		#	Your code here ...
+		#	
+        pass	
+
+
+
+
